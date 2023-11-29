f6165df0e677b43f87132684e989a33617146e85
Ariela Israel <arielaisrael12@gmail.com>
2023-11-29 01:54:02 -0500

    lesson complete

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,18 @@
-function intersection(a, b) {}
+function intersection(a, b) {
+	const map = new Map();
+	const set = new Set();
+
+	for (let e of a) {
+		map.set(e, e);
+	}
+
+	for (let e of b) {
+		if (map.has(e)) {
+			set.add(e);
+		}
+	}
+
+	return Array.from(set);
+}
 
 module.exports = intersection;
--- src/sameLetters/index.js
+++ src/sameLetters/index.js
@@ -2,6 +2,22 @@
  * return a Map of the unique letters and arrays of words that are made up of those letters
  * @param {array} words An array  of words
  */
-function sameLetters(words) {}
+function sameLetters(words) {
+	const results = new Map();
+
+	for (let word of words) {
+		let letters = new Set(word.toLowerCase());
+		let lettersArr = [...letters].sort();
+		let sortedWord = lettersArr.join('');
+  
+    if (results.has(sortedWord)) {
+      results.get(sortedWord).push(word)
+    } else {I
+      results.set(sortedWord, [word])
+    }
+  }
+
+	return results;
+}
 
 module.exports = sameLetters;
